0aa3cc9027e56d546e1d56035886b2e83a0784d3
Johannes Wilde <git@jowil.de>
2022-03-19 22:05:39 +0100

    Make message selectable. - The string can be changed by long-pressing the button. - If long-pressed the LED will light up, and shortly turn off with each consecutive button press. - When the button is released for a certain time, the LED will turn off fully and the newly selected string selected.

--- main.cpp
+++ main.cpp
@@ -29,8 +29,41 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+#include <avr/pgmspace.h>
 #include <avr/sleep.h>
 
+#include <string.h>
+
+
+namespace Messages
+{
+
+namespace Internal
+{
+
+static size_t constexpr maximumMessageLength = 50;
+
+char const string_0[] PROGMEM = "String 0";
+char const string_1[] PROGMEM = "String 1";
+char const string_2[] PROGMEM = "String 2";
+char const string_3[] PROGMEM = "String 3";
+char const string_4[] PROGMEM = "String 4";
+char const string_5[] PROGMEM = "String 5";
+
+char const * const array[] PROGMEM = {string_0, string_1, string_2, string_3, string_4, string_5};
+
+} // namespace Internal
+
+static size_t constexpr count = sizeof(Internal::array) / sizeof(Internal::array[0]);
+
+char const * getMessage(size_t const index)
+{
+    static char buffer[Internal::maximumMessageLength + 1] = {0};
+    strncpy_P(buffer, (char *)pgm_read_word(&(Internal::array[index])), Internal::maximumMessageLength);
+    return buffer;
+}
+
+} // namespace Messages
 
 namespace Pins
 {
@@ -43,6 +76,9 @@ static uint8_t constexpr led = 11;
 static Keyboard_ & slowKeyboard = Keyboard;
 
 static bool volatile buttonPressed = false;
+
+static size_t messageIndex = 0;
+
 
 static void isrAwake()
 {
@@ -69,6 +105,7 @@ void setup()
 
     slowKeyboard.begin(KeyboardLayout_de_DE);
 
+    // Wait for the USB connection to become operational.
     delay(600);
 
     while (true)
@@ -77,8 +114,78 @@ void setup()
 
         if (buttonPressed)
         {
-            slowKeyboard.print("Hello, World! or something other very extensive one should know about...");
-            Keyboard.write(KEY_RETURN);
+            bool longPress = false;
+            {
+                unsigned long const timePressed = millis();
+                delay(50);
+                while (LOW == digitalRead(Pins::button))
+                {
+                    // wait for the button to be released
+                    unsigned long const now = millis();
+                    if (250 < (now - timePressed))
+                    {
+                        longPress = true;
+                        digitalWrite(Pins::led, HIGH);
+                    }
+                }
+            }
+
+            if (longPress)
+            {
+                // change the selected message
+
+                unsigned long constexpr timeout = 2000;
+                bool timedOut = false;
+                size_t buttonPresses = 0;
+
+                do
+                {
+                    {
+                        // check whether pressed again
+                        unsigned long const timeReleased = millis();
+                        delay(50);
+                        while ((HIGH == digitalRead(Pins::button)) && !timedOut)
+                        {
+                            if (timeout < (millis() - timeReleased))
+                            {
+                                timedOut = true;
+                            }
+                        }
+                    }
+
+                    if (!timedOut)
+                    {
+                        // wait to be released again
+                        unsigned long const timePressed = millis();
+                        digitalWrite(Pins::led, LOW);
+                        delay(50);
+                        while ((LOW == digitalRead(Pins::button)))
+                        {
+                            if (500 < (millis() - timePressed))
+                            {
+                                digitalWrite(Pins::led, HIGH);
+                            }
+                        }
+                        digitalWrite(Pins::led, HIGH);
+
+                        buttonPresses = (buttonPresses + 1) % Messages::count;
+                    }
+                }
+                while (!timedOut);
+
+                digitalWrite(Pins::led, LOW);
+
+                if (0 < buttonPresses)
+                {
+                    messageIndex = buttonPresses - 1;
+                }
+            }
+            else
+            {
+                // write out the message for a short press of the button
+                slowKeyboard.print(Messages::getMessage(messageIndex));
+                Keyboard.write(KEY_RETURN);
+            }
 
             buttonPressed = false;
         }
